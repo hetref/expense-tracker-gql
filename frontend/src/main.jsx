1d28f5c32f94e58ceca261e65c30f365bc041f25
Aryan shinde <shindearyan179@gmail.com>
2024-03-17 20:31:36 +0530

    UI Design added

---
+++ frontend/src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import GridBackground from "./components/ui/GridBackground.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <GridBackground>
+        <App />
+      </GridBackground>
+    </React.StrictMode>
+  </BrowserRouter>
+);
