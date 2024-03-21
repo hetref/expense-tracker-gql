111b3cc6b62213d8b61d0aeceddd29d438f12205
Aryan shinde <shindearyan179@gmail.com>
2024-03-22 00:52:53 +0530

    CRUD added to Client Frontend

--- frontend/src/components/TransactionForm.jsx
+++ frontend/src/components/TransactionForm.jsx
@@ -1,4 +1,12 @@
+import { useMutation } from "@apollo/client";
+import { CREATE_TRANSACTION } from "../graphql/mutations/transaction.mutation";
+import toast from "react-hot-toast";
+
 const TransactionForm = () => {
+  const [createTransaction, { loading }] = useMutation(CREATE_TRANSACTION, {
+    refetchQueries: ["GetTransactions"],
+  });
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -12,6 +20,20 @@ const TransactionForm = () => {
       location: formData.get("location"),
       date: formData.get("date"),
     };
+
+    try {
+      await createTransaction({
+        variables: {
+          input: transactionData,
+        },
+      });
+      form.reset();
+      toast.success("Transaction added successfully");
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+
     console.log("transactionData", transactionData);
   };
 
@@ -159,8 +181,9 @@ const TransactionForm = () => {
           from-pink-500 to-pink-500 hover:from-pink-600 hover:to-pink-600
 						disabled:opacity-70 disabled:cursor-not-allowed"
         type="submit"
+        disabled={loading}
       >
-        Add Transaction
+        {loading ? "Loading..." : "Add Transaction"}
       </button>
     </form>
   );
